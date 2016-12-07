c48ca2fdc9aaccde9766c5548927915bc12a4c94
papapin <borabora1212>
2016-12-07 18:02:33 +0200

    step 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
+
   title = 'app works!';
+  size = 0;
+  show: string = 'none';
+
+  workBelt(): any {
+    this.size = -100;
+    this.show = 'block';
+  }
+
+  returnWorkBelt(){
+    this.size = 0;
+    this.show = 'none';
+  }
+
+
+
+
+
 }
